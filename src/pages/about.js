db6de79466a6d83256bd582c8e352af09faa2b8c
Coderganesh <108095949+Ganeshshit@users.noreply.github.com>
2023-04-30 12:04:54 +0530

    update

--- src/pages/about.js
+++ src/pages/about.js
@@ -41,12 +41,12 @@ const About = () => {
                     <div className='grid w-full grid-cols-8 '>
                         <div className=' col-span-3 flex flex-col items-start justify-start xl:col-span-4 md:order-2 md:col-span-8'>
                             <h2 className=' mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light sm:mt-5 md:mt-5 xl:mt-5'>Biography</h2>
-                            <p className=' font-medium text-2xl'>Hi, I'm Ganesh, a web developer and UI/UX designer with a passion for creating beautiful, functional,
+                            <p className=' font-medium text-2xl'>Hi, I am Ganesh, a web developer and UI/UX designer with a passion for creating beautiful, functional,
                                 and user-centered digital experiences. With 4 years of experience in the field. I am always looking for
-                                new and innovative ways to bring my clients' visions to life.</p>
-                            <p className=' font-medium text-2xl' >I believe that design is about more than just making things look pretty  it's about solving problems and
+                                new and innovative ways to bring my clients visions to life.</p>
+                            <p className=' font-medium text-2xl' >I believe that design is about more than just making things look pretty  it is about solving problems and
                                 creating intuitive, enjoyable experiences for users. </p>
-                            <p className=' font-medium text-2xl'>Whether I'm working on a website, mobile app, or
+                            <p className=' font-medium text-2xl'>Whether I amm working on a website, mobile app, or
                                 other digital product, I bring my commitment to design excellence and user-centered thinking to
                                 every project I work on. I look forward to the opportunity to bring my skills and passion to your next project.</p>
                         </div>
