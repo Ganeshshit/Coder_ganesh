71d8621191b81953fe7dfbb23f57e163ab5602b9
Coderganesh <108095949+Ganeshshit@users.noreply.github.com>
2023-09-13 12:53:48 +0000

    Project add

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -10,11 +10,13 @@ const FramerImage = motion(Image);
 import project1 from '../../public/images/projects/noteapp.png'
 import project2 from '../../public/images/projects/weather_time.png'
 import project3 from '../../public/images/projects/music.png'
+import Bitcoin from '../../public/images/projects/BitcoinBaat.png'
 
-const FeaturedProject = ({ type, title, summary, img, link, github }) => {
+const FeaturedProject = ({ type, title, summary, img, link, github, techStack, techStackName }) => {
   return (
     <article className='w-full flex items-center justify-between relative rounded-3xl border  border-solid border-dark bg-light shadow-2xl
     p-12 rounded-br-2xl 
+    mt-4
     '>
       <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark' />
       <Link href={link} target='_blank'
@@ -32,6 +34,13 @@ const FeaturedProject = ({ type, title, summary, img, link, github }) => {
           <h2 className='my-2 w-full text-left text-4xl  font-bold '>{title}</h2>
         </Link>
         <p className=' my-2 font-medium text-dark'>{summary}</p>
+        <h3> <b>
+          {techStack}
+        </b>
+        </h3>
+        <br />
+        <p>{techStackName}</p>
+
         <div className=' mt-2 flex items-center'>
           <Link href={github} target='_blank' className=' w-10 '><GithubIcon /></Link>
           <Link href={link} target='_blank'
@@ -48,6 +57,7 @@ const Project = ({ title, type, img, link, github }) => {
       <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark' />
       <Link href={link} target='_blank'
         className='w-full cursor-pointer overflow-hidden rounded-lg
+        
       '
       >
         <FramerImage src={img} alt='title' className='w-full h-auto'
@@ -58,7 +68,7 @@ const Project = ({ title, type, img, link, github }) => {
       <div className=' w-full flex flex-col items-start justify-between mt-4'>
         <span className=' text-primary font-medium text-xl'>{type}</span>
         <Link href={link} target='_blank' className=' hover:underline underline-offset-2'>
-          <h2 className='my-2 w-full text-left text-3xl  font-bold '>{title}</h2>
+          <h2 className='my-2 w-full text-left text-3xl  font-bold ml-2'>{title}</h2>
         </Link>
         <p className=' my-2 font-medium text-dark'></p>
         <div className=' mt-2 flex items-center'>
@@ -83,15 +93,16 @@ const Projects = () => {
           <div className=' grid grid-cols-12 gap-24 '>
             <div className=' col-span-12'>
               <FeaturedProject
-                title='Note Application'
-                summary='A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
-              It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
-              local currency.'
-                img={project1}
-                link='https://ganeshshit.github.io/notes/'
-                github='https://github.com/Ganeshshit/notes'
-                type='Crypto'
-                />
+
+                title='Cryptocurrency Dashboard'
+                summary='The Crypto Dashboard project aims to provide users with a comprehensive and visually appealing platform to track and analyze cryptocurrency market data. Leveraging the power of React, React Chart Library, Material-UI, and the CoinGecko API, this dashboard will empower users to make informed decisions in the volatile world of cryptocurrencies.'
+                img={Bitcoin}
+                link='https://deshboard-94nkyk2kw-ganeshshit.vercel.app/'
+                github='https://github.com/Ganeshshit/deshboard'
+
+                techStack='TechStack'
+                techStackName="React, Meterial UI ,React Charting Library,CoinGekoAPI"
+              />
             </div>
             <div className=' col-span-6'>
               <Project
@@ -99,7 +110,7 @@ const Projects = () => {
                 img={project1}
                 link='https://ganeshshit.github.io/notes/'
                 github='https://github.com/Ganeshshit/notes'
-                type='Note app'
+
               />
             </div>
             <div className=' col-span-6'>
@@ -108,26 +119,9 @@ const Projects = () => {
                 img={project2}
                 link='https://ganeshshit.github.io/weather_time1/'
                 github='https://github.com/Ganeshshit/notes'
-                type='Weather App'
               />
             </div>
             <div className=' col-span-12'>
-            <FeaturedProject
-                title='Music Application'
-                summary='A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
-              It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
-              local currency.'
-                img={project3}
-                link='https://ganeshshit.github.io/newmusic/'
-                github='https://github.com/Ganeshshit/newmusic/settings/pages'
-                type='Music'
-                />
-            </div>
-            <div className=' col-span-6'>
-              Project-3
-            </div>
-            <div className=' col-span-6'>
-              Project-4
             </div>
           </div>
         </Layout>
